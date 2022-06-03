b34a72a07f82b5f433e941496d6f68f7ba561945
Delisha Cogdell <ceedeelight@yahoo.com>
2022-06-02 20:03:13 -0400

    solution027.js

---
+++ solution027.js
@@ -0,0 +1,10 @@
+/*In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?  
+-Math.abs is what's needed.
+
+function makeNegative(num) {
+   return(-Math.abs(42) )   
+  } */
+
+  function makeNegative(num) {
+    return(-Math.abs(num) )     ///I removed the number 42 and put in num.
+   }
