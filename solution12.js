11652c62fb38d0e22fd9b7c78c428bd9ed310b00
Delisha Cogdell <ceedeelight@yahoo.com>
2022-08-03 16:40:27 -0400

    transform a num to a string

---
+++ solution12.js
@@ -0,0 +1,7 @@
+/* We need a function that can transform a number into a string.*/
+
+function numberToString(num) {
+    return (num.toString())     // Return a string of the number here!
+    
+}
+
