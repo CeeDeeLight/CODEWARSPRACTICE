28526ae10c8f319fb48b941f3b7cf7ae3109502e
Delisha Cogdell <ceedeelight@yahoo.com>
2022-08-04 13:30:57 -0400

    Take a given num and make it negative

---
+++ solution13.js
@@ -0,0 +1,8 @@
+/*In this simple assignment, you are given a number and have to make it negative. But maybe the number is already negative?
+Examples
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5 */
+
+function makeNegative(num) {
+    return(-Math.abs(42) )
+} 
