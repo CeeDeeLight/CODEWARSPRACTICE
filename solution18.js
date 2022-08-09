78d7edbc3f4acbda8600e274d00e528a87ce1aa4
Delisha Cogdell <ceedeelight@yahoo.com>
2022-08-09 07:33:11 -0400

    function does four basic mathematical operations

---
+++ solution18.js
@@ -0,0 +1,14 @@
+/*Create a function that does four basic mathematical operations.*/
+
+if( operation == "+") {
+    return value1 + value2
+  }
+  if( operation == "-") {
+    return value1 - value2
+  }
+  if( operation == "*") {
+    return value1 * value2
+  }
+  if( operation == "/") {
+    return value1 / value2
+  }
