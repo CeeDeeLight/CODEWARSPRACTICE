13266608079dd1db29a1032d14dde10fba2c07da
Delisha Cogdell <ceedeelight@yahoo.com>
2022-08-23 08:29:25 -0400

    Remove spaces from string and return resultant string

---
+++ solution33.js
@@ -0,0 +1,5 @@
+/*remove the spaces from the string, then return the resultant string.*/
+
+function noSpace(x){
+    return x.replace (/ /g, ''); //use a regular expression and replace all spaces globally.
+}
