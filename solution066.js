1d6fa679d555c97dfd648771c3010936f7908608
Delisha Cogdell <ceedeelight@yahoo.com>
2022-09-25 08:37:18 -0400

    Math.floor

--- solution066.js
+++ solution066.js
@@ -1,3 +1,5 @@
 function mango(quantity, price){
     return price * (quantity - Math.floor(quantity / 3));
   }
+  //Math.floor function always rounds down and returns the largest integer less than or equal to a given number
+  
