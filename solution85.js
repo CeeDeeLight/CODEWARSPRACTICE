b0f6d75cefee3ea7cfcadf84617f94327cdba060
Delisha Cogdell <ceedeelight@yahoo.com>
2022-10-12 09:45:45 -0400

    two functs should be equiv. Can you spot the prob and fix it?

---
+++ solution85.js
@@ -0,0 +1,14 @@
+//two funcs getmax1 & getmax2 be equivalent and return the same value. Can you spot the prob. and fix it.
+
+function getMax1() {
+  var max = {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2(){
+ return {
+    name: 'Max Headroom'
+  }
+}
