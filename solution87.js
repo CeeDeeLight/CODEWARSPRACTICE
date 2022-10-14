17619a8cb854be14b0c87e13f0d88f34e08c5ec8
Delisha Cogdell <ceedeelight@yahoo.com>
2022-10-14 13:17:52 -0400

    A Function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not.

---
+++ solution87.js
@@ -0,0 +1,4 @@
+//function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not.
+function plural(n) {
+return n !=1;
+  }
